d2cc6630af8b8ab52dcf19ad070f19b2acbe6f29
Abdi Bekele <abdibekeleb94@gmail.com>
2025-12-08 21:48:29 +0300

    feat(server): implement comments functionality for articles

--- server.js
+++ server.js
@@ -2,8 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+const articlesInfo = {
+  "mastering-react-basics": {
+    comments: [],
+  },
+  "build-node-api-quickly": {
+    comments: [],
+  },
+  "react-learning-journey": {
+    comments: [],
+  },
+  "fullstack-mern-overview": {
+    comments: [],
+  },
+};
+
+app.use(express.json({ extended: false }));
+
+app.post("/api/articles/:name/add-comments", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
 });
 
 app.listen(PORT, () => {
